cbdc56088d711ab46d70e0ed130c9e34f0e5678d
Kane <kanelariviere@outlook.com>
2023-06-23 11:54:43 -0400

    styled map & info; added home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Map from '@/components/map'
+import Home from '@/components/home'
 import Image from 'next/image'
 
 
-export default function Home() {
+export default function Page() {
   return (
     <>
+      <Home />
       <Map />
     </>
   )
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,14 +1,15 @@
 'use client'
 import React, { useState } from 'react';
+import Link from 'next/link'
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 import usStatesData from '../data/us-states-output.json';
 import usStatesStatsData from '../data/us-states-stats.json';
 
 const Legend = ({ hoveredStateColor }: { hoveredStateColor: string }) => {
   return (
-    <div className="p-4 flex justify-start">
+    <div className="p-4">
       <div
-        className="w-20 h-20 rounded-full"
+        className="w-10 h-10 rounded-full"
         style={{
           // style the box based on the hovered state color
           backgroundColor: hoveredStateColor,
@@ -26,10 +27,19 @@ const StateInformation = ({ stateId, selectedDataSet }: { stateId: string, selec
   if (stateData) {
     const { per, total } = stateData.dates[selectedDataSet as keyof typeof stateData.dates];
     return (
-      <div className="">
-        <h2>{stateData.name}</h2>
-        <p>{per} per 100,000</p>
-        <p>{total}</p>
+      <div className="flex flex-col w-full">
+        <div className="flex items-center justify-between">
+          <span className="text-xs text-neutral-300 uppercase">state</span>
+          <h2 className="text-xs text-neutral-500 uppercase font-semibold">{stateData.name}</h2>
+        </div>
+        <div className="flex items-center justify-between uppercase">
+          <span className="text-xs text-neutral-300">rate</span>
+          <h2 className="text-xs text-neutral-500 font-semibold">{per}</h2>
+        </div>
+        <div className="flex items-center justify-between uppercase">
+          <span className="text-xs text-neutral-300">total</span>
+          <h2 className="text-xs text-neutral-500 font-semibold">{total}</h2>
+        </div>
       </div>
     );
   }
@@ -46,41 +56,32 @@ const StateMap = () => {
   const [selectedDataSet, setSelectedDataSet] = useState('2021');
   const [hoveredState, setHoveredState] = useState('');
   const [hoveredStateColor, setHoveredStateColor] = useState('#ccc');
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
   const calculateFillColor = (stateId: string) => {
     const stateData = usStatesStatsData.find((data) => data.name === stateId);
     if (stateData && stateData.dates && stateData.dates[selectedDataSet as keyof typeof stateData.dates]) {
       const { per: statePer, total } = stateData.dates[selectedDataSet as keyof typeof stateData.dates];
-      // Calculate the fill color based on the "per" value
-      const maxPer = 33;
-      const minPer = 7;
-      const normalizedPer = (statePer - minPer) / (maxPer - minPer);
-      const startColor = '#99f6e4'; // Pastel teal
-      const endColor = '#075985'; // Teal
-      const r =
-        Math.round(
-          parseInt(startColor.substr(1, 2), 16) +
-            normalizedPer *
-              (parseInt(endColor.substr(1, 2), 16) -
-                parseInt(startColor.substr(1, 2), 16))
-        );
-      const g =
-        Math.round(
-          parseInt(startColor.substr(3, 2), 16) +
-            normalizedPer *
-              (parseInt(endColor.substr(3, 2), 16) -
-                parseInt(startColor.substr(3, 2), 16))
-        );
-      const b =
-        Math.round(
-          parseInt(startColor.substr(5, 2), 16) +
-            normalizedPer *
-              (parseInt(endColor.substr(5, 2), 16) -
-                parseInt(startColor.substr(5, 2), 16))
-        );
-      return `rgb(${r}, ${g}, ${b})`;
+  
+      if (statePer >= 0 && statePer <= 5) {
+        return '#fff';
+      } else if (statePer > 6 && statePer <= 10) {
+        return '#ffedd5';
+      } else if (statePer > 10 && statePer <= 15) {
+        return '#fed7aa';
+      } else if (statePer > 15 && statePer <= 20) {
+        return '#fb923c';
+      } else if (statePer > 20 && statePer <= 25) {
+        return '#ef4444';
+      } else if (statePer > 25 && statePer <= 30) {
+        return '#075985';
+      } else if (statePer > 30 && statePer <= 35) {
+        return '#082f49';
+      } else {
+        // Default fill color for states outside the defined ranges
+        return '#ccc';
+      }
     }
+  
     // Default fill color for states without data
     return '#ccc';
   };
@@ -88,7 +89,6 @@ const StateMap = () => {
   const handleStateHover = (stateId: string, fillColor: string, event: MouseEvent) => {
     setHoveredState(stateId);
     setHoveredStateColor(fillColor);
-    setCursorPosition({ x: event.pageX, y: event.pageY });
   };
 
   const handleStateLeave = () => {
@@ -100,11 +100,12 @@ const StateMap = () => {
     setSelectedDataSet(dataSet);
   };
 
+  
   return (
-    <div className="max-w-5xl mx-auto relative">
+    <div className="max-w-5xl mx-auto relative border-b py-4">
 
       {/* Data Set Toggle */}
-      <div className="flex justify-center mt-4 text-xs">
+      <div className="flex justify-center mt-4 text-xs border-t p-4">
         <button
           className={`mx-2 ${
             selectedDataSet === '2017' ? 'text-sky-700 underline underline-offset-8' : ''
@@ -172,26 +173,65 @@ const StateMap = () => {
       </ComposableMap>
 
       {/* Legend */}
-      <div className="p-2 w-full">
-        <div className="flex justify-between text-xs text-neutral-500 mb-2">
-          <span>7</span>
-          <span>33</span>
-        </div>
-        <div className="border h-2 border-[#222] bg-gradient-to-r from-[#99f6e4] to-[#075985]"></div>
-      </div>
-
-      {/* Info Box */}
-      {hoveredState && (
-        <div
-          className="absolute bg-white rounded-lg p-4 border flex items-center"
-          style={{ left: cursorPosition.x, top: cursorPosition.y }}
-        >
-          <Legend hoveredStateColor={hoveredStateColor} />
-          <div className="h-20 rounded w-48 flex items-center">
-            <StateInformation stateId={hoveredState} selectedDataSet={selectedDataSet} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-12 pb-2">
+        <div className="p-2">
+          <div className="flex justify-start flex-wrap gap-x-8 gap-y-4">
+            <div className="flex items-center space-x-2">
+              <div className="h-5 w-5 bg-white border rounded-full"></div>
+              <span className="text-xs">0 &mdash; 5</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="h-5 w-5 bg-[#ffedd5] rounded-full"></div>
+              <span className="text-xs">6 &mdash; 10</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="h-5 w-5 bg-[#fed7aa] rounded-full"></div>
+              <span className="text-xs">11 &mdash; 15</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="h-5 w-5 bg-[#fb923c] rounded-full"></div>
+              <span className="text-xs">16 &mdash; 20</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="h-5 w-5 bg-[#ef4444] rounded-full"></div>
+              <span className="text-xs">21 &mdash; 25</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="h-5 w-5 bg-[#075985] rounded-full"></div>
+              <span className="text-xs">26 &mdash; 30</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="h-5 w-5 bg-[#082f49] rounded-full"></div>
+              <span className="text-xs">31 &mdash; 33</span>
+            </div>
           </div>
         </div>
-      )}
+
+        {/* Info Box */}
+        {hoveredState && (
+          <div className="flex items-center bg-white rounded-md border border-neutral-300 my-2 mx-2 h-14">
+            <Legend hoveredStateColor={hoveredStateColor} />
+            <div className=" w-full px-4">
+              <StateInformation stateId={hoveredState} selectedDataSet={selectedDataSet} />
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between space-y-2 p-2">
+        <p className="text-xs">
+          &#x2a; Rates shown are per 100,000 of total population. 
+        </p>
+        <p className="text-xs">
+          &#x2a;&nbsp;
+          <span className="underline underline-offset-4">Source</span> 
+          :
+          <Link href="https://www.cdc.gov/nchs/pressroom/sosmap/suicide-mortality/suicide.htm" target="_blank"
+            className="hover:text-blue-400 transition duration-200">
+            &nbsp;CDC.GOV
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
